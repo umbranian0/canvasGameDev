08c34bb11e465ac1cb48c25883ec4eb3a8c57eec
vasile timotin <vasiletimotin@outlook.pt>
2019-06-21 20:50:42 +0100

    Update mainFunctionality.js

--- mainFunctionality.js
+++ mainFunctionality.js
@@ -41,10 +41,6 @@
 
 	//degbug
 	var debugMode = true;
-<<<<<<< HEAD
-=======
-	var andou = false;
->>>>>>> f4a85da7bbe0107ba07dfeb229e86dd9ec94c1e6
 
 	//componenetes do jogo
 	var barraEnergiaEnemy = undefined;
